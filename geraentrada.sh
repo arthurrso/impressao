92541622e858f975d083b7bb284cf49d38ab93a9
danilotorres9 <danilo.costa@ccc.ufcg.edu.br>
2018-11-12 16:25:22 -0300

    Alterando o gerador de entrada

--- geraentrada.sh
+++ geraentrada.sh
@@ -1,1 +1,3 @@
-who | awk '{print $1 ";0;0;30"}' | sort | uniq -u > entrada.txt
+#24 16 * * * bash /home/danilotc/Documentos/ADM-SYSTEM/Impressao/geraentrada.sh
+
+who | awk '{print $1 ";0;0;30"}' | sort | uniq -u >> entrada.txt
